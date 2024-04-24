24faacba816405c9e56bad140d4649edcee066c7
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-04-24 12:57:40 -0400

    Removed keg_only

--- Formula/pnpm@8.rb
+++ Formula/pnpm@8.rb
@@ -22,8 +22,6 @@ class PnpmAT8 < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea36ce48c3435f17c6c3f59ca1f0113d7111f5ddb15ed665d2b2265fb374e816"
   end
 
-  keg_only :versioned_formula
-
   depends_on "node" => [:build, :test]
 
   def install
