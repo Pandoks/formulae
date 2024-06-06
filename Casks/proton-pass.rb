d5a2047af7084261f2c96e2fa3a5c110ba633cf8
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-06-06 13:07:16 -0700

    Styling

--- Casks/proton-pass.rb
+++ Casks/proton-pass.rb
@@ -18,11 +18,11 @@ cask "proton-pass" do
   app "Proton Pass.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/me.proton.pass.electron.*",
     "~/Library/Application Support/Proton Pass",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/me.proton.pass.electron.sfl3",
     "~/Library/Caches/me.proton.pass.electron.ShipIt",
     "~/Library/HTTPStorages/me.proton.pass.electron",
     "~/Library/Logs/Proton Pass",
-    "~/Library/Preferences/me.proton.pass.electron.plist"
+    "~/Library/Preferences/me.proton.pass.electron.plist",
   ]
 end
